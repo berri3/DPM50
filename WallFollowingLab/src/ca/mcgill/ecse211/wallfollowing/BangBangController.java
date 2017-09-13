24f6f8de29f1a023d7464b297c98b846a3f65fc2
berri3 <cmliang2004@hotmail.com>
2017-09-13 16:07:19 -0400

    Changed back some of the speeds

--- WallFollowingLab/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ WallFollowingLab/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -7,10 +7,11 @@ public class BangBangController implements UltrasonicController {
   private final int bandCenter; //constant offset from the wall
   private final int bandwidth; //width of dead band (allowed error (?))
   private final int motorLow;
-  private final int motorHigh;
+  private final int motorHigh;  
   private int distance; //Distance from the wall(?)
-  private int distError; //
+  private int distError; //Calculated error from the measured distance
   private static final int DELTASPD = 75;
+  private static final int motorReallyHigh = 200;
 
   public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
     // Default Constructor
@@ -28,6 +29,13 @@ public class BangBangController implements UltrasonicController {
   public void processUSData(int distance) {
     this.distance = distance;
     distError = bandCenter - this.distance;
+    
+    if (distance > 200) { //really too far; probably because the wall is ending
+    	WallFollowingLab.leftMotor.setSpeed(motorReallyHigh);
+    	WallFollowingLab.rightMotor.setSpeed(motorReallyHigh);
+    	WallFollowingLab.leftMotor.backward();
+    	WallFollowingLab.rightMotor.forward();
+    }
     
     if (Math.abs(distError) <= bandwidth){
     	WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
